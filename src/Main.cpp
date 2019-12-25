c3bfc5305364bdf1d3a5c27300bb8e9f675eb9a7
MCS415 <mcsippel@protonmail.com>
2019-12-24 17:23:03 -0800

    simply frustrating make

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 
-void Log(const char* message);
+void Log(int a, int b);
 
+int Multiply(int a, int b)
+{
+    return a * b;
+}
+
+void MultiplyAndLog(int a, int b)
+{
+    int result = Multiply(a, b);
+    std::cout << result << std::endl;
+}
 int main()
 {
-    unsigned int variable = 8;
-    std::cout << variable << std::endl;
+    MultiplyAndLog(3,7);
+    MultiplyAndLog(9,2);
+    MultiplyAndLog(32,90);
     std::cin.get();
 }
